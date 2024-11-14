b5c71272e70a0736a57183f9389f368e0e22a7fc
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-11-15 01:52:43 +0600

    news details in progress

--- src/Pages/NewsDetails.jsx
+++ src/Pages/NewsDetails.jsx
@@ -1,27 +1,42 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import Header from "../components/Header";
 import RightNavbar from "../components/layout-components/RightNavbar";
 
 const NewsDetails = () => {
+  const data = useLoaderData();
+  const news = data.data[0]
 
-    const {data: news} = useLoaderData();
-    console.log(news[0]);
-
-    return (
-        <div>
-            <header>
-                <Header></Header>
-            </header>
-            <main className="w-11/12 mx-auto grid grid-cols-4 gap-5">
-            <section className="col-span-3">
-                <h2 className="font-semibold mb-3">Dragon News</h2>
-            </section>
-            <aside className="col-span-1">
-                <RightNavbar></RightNavbar>
-            </aside>
-            </main>
-        </div>
-    );
+  return (
+    <div>
+      <header>
+        <Header></Header>
+      </header>
+      <main className="w-11/12 mx-auto grid grid-cols-4 gap-5">
+        <section className="col-span-3">
+          <h2 className="font-semibold mb-3">Dragon News</h2>
+          <div className="card bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src={news?.image_url}
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{news.title}</h2>
+              <p>{news.details}</p>
+              <div className="card-actions">
+                <Link to={`/category/${news?.category_id}`} className="btn btn-primary">Back to Category</Link>
+              </div>
+            </div>
+          </div>
+        </section>
+        <aside className="col-span-1">
+          <RightNavbar></RightNavbar>
+        </aside>
+      </main>
+    </div>
+  );
 };
 
 export default NewsDetails;
